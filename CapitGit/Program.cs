58ecb49807f2b832936edb04acda8105c42653ff
Dvir <dvir16@gmail.com>
2020-08-13 09:02:05 +0300

    Add project files.

---
+++ CapitGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CapitGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
